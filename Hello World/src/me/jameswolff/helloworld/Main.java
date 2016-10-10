01bafb3137437009c50f056a728bd524bf1b9bb3
James Wolff <james.wolff0@gmail.com>
2016-10-10 16:46:35 -0500

    Final code for Part 2

---
+++ Hello World/src/me/jameswolff/helloworld/Main.java
@@ -0,0 +1,28 @@
+package me.jameswolff.helloworld;
+
+/**
+ * Created by James Wolff on 10/10/2016.
+ */
+public class Main {
+    /*
+     * This is a multi-line comment, we can talk about a lot of stuff in these.
+     */
+
+    // This is a single-line comment
+
+    /**
+     * If we add a second asterisk on the first line of this comment this
+     * becomes a JavaDoc comment which support HTML tags and can be generated
+     * and shipped along side yor library!
+     */
+    public static void main(String[] /*arguments*/ args) { // We can use multi-line comments in the middle of code to test renaming a variable, etc. Single line comments can also be appended to the end of a line of code!
+        System.out.print("He");
+        System.out.print("l");
+        System.out.print("l");
+        System.out.print("o W");
+        System.out.print("o");
+        System.out.print("rld");
+        System.out.println("!");
+        System.out.println("Hello World!");
+    }
+}
